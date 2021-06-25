98b0f35cdff748c544440db3e246c0bde2e3060b
Elton Possidonio <eltonrpo@gmail.com>
2021-06-25 10:08:31 -0300

    Update ContaBancaria.java

--- SimulacaoBancaria/src/simulacaobancaria/ContaBancaria.java
+++ SimulacaoBancaria/src/simulacaobancaria/ContaBancaria.java
@@ -9,50 +9,78 @@ public class ContaBancaria {
     private float saldo;
     private boolean ativa;
     
-    // Métodos
-    public int getNumConta() {
-        return numConta;
+    // Métodos Personalizados
+    public void abrirConta(String tipo){
+        this.setTipo(tipo);
+        this.setAtiva(true);
+        if ("CC".equals(tipo)) {
+            this.setSaldo(50);
+        } else if ("CP".equals(tipo)){
+            this.setSaldo(150);
+        }
+        System.out.println("Conta aberta com sucesso!");
     }
-
-    public void setNumConta(int numConta) {
-        this.numConta = numConta;
+    
+    public void fecharConta(){
+        
     }
-
-    public String getTipo() {
-        return tipo;
+    
+    public void depositar() {
+        
     }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
+    
+    public void sacar() {
+        
     }
-
-    public String getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(String cliente) {
-        this.cliente = cliente;
-    }
-
-    public float getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(float saldo) {
-        this.saldo = saldo;
-    }
-
-    public boolean isAtiva() {
-        return ativa;
-    }
-
-    public void setAtiva(boolean status) {
-        this.ativa = status;
+    
+    public void pagarMensal() {
+        
     }
     
     // Método Construtor
     public ContaBancaria () {
-        saldo = 0;
-        ativa = false;
+        this.setSaldo(0);
+        this.setAtiva(false);
     }
-}
+    
+    // Métodos Getters e Setters
+    public int getNumConta() {
+        return numConta;
+    }
+    
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+    
+    public String getTipo() {
+        return tipo;
+    }
+    
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    public String getCliente() {
+        return cliente;
+    }
+    
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+    
+    public float getSaldo() {
+        return saldo;
+    }
+    
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    
+    public boolean isAtiva() {
+        return ativa;
+    }
+    
+    public void setAtiva(boolean status) {
+        this.ativa = status;
+    }
+} // fim da classe
