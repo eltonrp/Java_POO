a8a1403deaedc1f91ef16c9662f14097a291fffd
Elton Possidonio <eltonrpo@gmail.com>
2021-07-06 10:16:50 -0300

    classe Luta

--- lutadores/src/lutadores/Lutadores.java
+++ lutadores/src/lutadores/Lutadores.java
@@ -8,10 +8,12 @@ public class Lutadores {
         l[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
         l[2] = new Lutador("Snapshadow", "EUA", 35, 1.65f, 80.9f, 12, 2, 1);
         l[3] = new Lutador("Dead Code", "Austrália", 28, 1.93f, 81.6f, 13, 0, 2);
-        l[4] = new Lutador("UFOCobol", "Brasil", 37, 1,70f, 119.3f, 5, 4, 3);
+        l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
         l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
-        l[0].apresentar();
-        l[1].status();
+        
+        Luta UFC01 = new Luta();
+        UFC01.marcarLuta(l[0], l[1]);
+        UFC01.lutar();
     }
     
 }
