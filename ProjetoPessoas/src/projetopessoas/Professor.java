37a94175a01f3563d6143d587b044440c2986b2c
Elton Possidonio <70237633+eltonrp@users.noreply.github.com>
2021-07-07 15:44:18 -0300

    Update Professor.java

--- ProjetoPessoas/src/projetopessoas/Professor.java
+++ ProjetoPessoas/src/projetopessoas/Professor.java
@@ -1,5 +1,25 @@
 package projetopessoas;
 
-public class Professor {
-    
+public class Professor extends Pessoa {
+    // Atributos
+    private String especialidade;
+    private float salario;
+
+    // Métodos Públicos
+    public receberAumento(float aum) {
+        this.salario += aum;
+    }
+    // Métodos Especiais
+    public String getEspecialidade() {
+        return this.especialidade;
+    }
+    public void setEspecialidade(String espec) {
+        this.especialidade = espec;
+    }
+    public float getSalario() {
+        return this.salario;
+    }
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
 }
