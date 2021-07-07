084fe7617e506f3a7af49b00fcee6fbfb833e140
Elton Possidonio <70237633+eltonrp@users.noreply.github.com>
2021-07-07 15:23:04 -0300

    Update Aluno.java

--- ProjetoPessoas/src/projetopessoas/Aluno.java
+++ ProjetoPessoas/src/projetopessoas/Aluno.java
@@ -1,5 +1,24 @@
 package projetopessoas;
 
-public class Aluno {
-    
+public class Aluno extends Pessoa {
+    private int mat;
+    private String curso;
+
+    // Métodos Públicos
+    public void cancelarMat() {
+        System.out.println("A matrícula será cancelada");
+    }
+    // Métodos Especiais
+    public int getMat() {
+        return this.mat;
+    }
+    public void setMat(int mat) {
+        this.mat = mat;
+    }
+    public String getCurso() {
+        return this.curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
 }
