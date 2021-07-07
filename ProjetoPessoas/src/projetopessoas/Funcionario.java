fecdee205b03d4326d875e3901e37c5ddaa4a895
Elton Possidonio <70237633+eltonrp@users.noreply.github.com>
2021-07-07 16:00:34 -0300

    Update Funcionario.java

--- ProjetoPessoas/src/projetopessoas/Funcionario.java
+++ ProjetoPessoas/src/projetopessoas/Funcionario.java
@@ -1,5 +1,25 @@
 package projetopessoas;
 
-public class Funcionario {
-    
+public class Funcionario extends Pessoa {
+    // Atributos
+    private String setor;
+    private boolean trabalhando;
+
+    // Métodos Públicos
+    public void mudaTrabalho() {
+        this.trabalhando = !this.trabalhando;
+    }
+    // Métodos Especiais
+    public String getSetor() {
+        return this.setor;
+    }
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+    public boolean isTrabalhando() {
+        return this.trabalhando;
+    }
+    public void setTrabalhando(boolean trab) {
+        this.trabalhando = trab;
+    }
 }
