8eb24a43e58d201a6faddbebe1e0daf8424ead23
Elton Possidonio <70237633+eltonrp@users.noreply.github.com>
2021-07-07 14:44:15 -0300

    Update Pessoa.java

--- ProjetoPessoas/src/projetopessoas/Pessoa.java
+++ ProjetoPessoas/src/projetopessoas/Pessoa.java
@@ -1,5 +1,33 @@
 package projetopessoas;
 
 public class Pessoa {
-    
+    // Atributos
+    private String nome;
+    private int idade;
+    private String sexo;
+
+    // Métodos Publicos
+    public void fazerAniversario() {
+        this.idade++;
+    }
+
+    // Métodos Especiais
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return this.idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getSexo() {
+        return this.sexo;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
 }
