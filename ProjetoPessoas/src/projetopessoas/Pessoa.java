e9ed7cbe297cb04a2586cfce1cb3a1a426d8f77b
Elton Possidonio <70237633+eltonrp@users.noreply.github.com>
2021-07-07 16:08:55 -0300

    Update

--- ProjetoPessoas/src/projetopessoas/Pessoa.java
+++ ProjetoPessoas/src/projetopessoas/Pessoa.java
@@ -30,4 +30,8 @@ public class Pessoa {
     public void setSexo(String sexo) {
         this.sexo = sexo;
     }
+    @Override
+    public String toString() {
+        return "Pessoa{"+"nome: "+nome+", idade: "+idade+", sexo: "+sexo+"}";
+    }
 }
